a8f398054aec64579ba0b165c47eb48c642c20cd
mmarew <35652680+mmarew@users.noreply.github.com>
2023-01-05 21:54:21 +0300

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyB4DrJzLfJ_fizBAAEdm2oBBCkPPdPb6m8",
-  authDomain: "cloned-e.firebaseapp.com",
-  projectId: "cloned-e",
-  storageBucket: "cloned-e.appspot.com",
-  messagingSenderId: "650852041837",
-  appId: "1:650852041837:web:75d35e0ac759068ceceb64",
-  measurementId: "G-1EBQ9MNP7F",
-}; 
+  apiKey: "AIzaSyDivQi0JcTM2rBpBiZOiVeWTA54fSkQ654",
+  authDomain: "e-clone-bee75.firebaseapp.com",
+  projectId: "e-clone-bee75",
+  storageBucket: "e-clone-bee75.appspot.com",
+  messagingSenderId: "576148544431",
+  appId: "1:576148544431:web:dfe010601237b5c3146d5a",
+  measurementId: "G-0NVQKB050D",
+};
 // Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 // Use these for db & auth
